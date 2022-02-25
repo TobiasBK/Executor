88f9b3cff953d63b8541cf32d2e2f7726be3937c
Tobias <thethirdpremise@gmail.com>
2022-02-25 16:08:15 -0500

    update contract

--- src/Executor.sol
+++ src/Executor.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.6 <0.9.0;
 
 /**
- * @dev A contract that allows the owner to call another contract using .call().
+ * @dev A contract that allows the owner to call another contract using .call
  */
 contract Executor {
     address private owner;
@@ -15,19 +15,19 @@ contract Executor {
     }
 
     /**
-     * @dev Sets creator address of the contract as owner on setup.
+     * @dev Sets creator address of the contract as owner on setup
      */
     constructor() {
         owner = msg.sender;
     }
 
     /**
-     * @dev Fallback function recevies any ether sent to this contract.
+     * @dev Fallback function recevies any ether sent to this contract
      */
     fallback() external payable {}
 
     /**
-     * @dev Allows owner to withdraw all ether.
+     * @dev Allows owner to withdraw all ether
      */
     function withdraw() external onlyOwner {
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
@@ -35,10 +35,9 @@ contract Executor {
     }
 
     /**
-     * @dev Solidity's .call() is a low-level function for interacting with other smart contracts.
-     * This is a great way to interface with smart contracts without the need for an ABI.
+     * @dev Solidity's .call is a low-level function for interacting with other smart contracts
+     * This is a great way to interface with smart contracts without the need for an ABI
      */
-
     function executor(address _externalAddr, bytes memory _externalData)
         public
         payable
